0d68e3e208cf415ed91079b6d81c898138b8b8af
JYStudios <joshuayin29@gmail.com>
2026-08-19 12:42:12 -0400

    enable remote reporting period dropdown

--- apps/dashboard/src/main.jsx
+++ apps/dashboard/src/main.jsx
@@ -11,6 +11,7 @@ import "./metric-customization.css";
 import "./spreadsheet-slide.css";
 import "./measuredSlidingHighlights";
 import "./remote-polish";
+import "./remote-period-dropdown";
 import { DisplayErrorBoundary } from "./components/DisplayErrorBoundary";
 
 createRoot(document.getElementById("root")).render(<StrictMode><DisplayErrorBoundary><PresentationControllerProvider><App /></PresentationControllerProvider></DisplayErrorBoundary></StrictMode>);
