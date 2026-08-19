753cc69a5ddfc3d82091a8bfe252101d7c51e81c
JYStudios <joshuayin29@gmail.com>
2026-08-19 12:57:05 -0400

    load actual remote period and mobile fixes

--- apps/dashboard/src/main.jsx
+++ apps/dashboard/src/main.jsx
@@ -11,6 +11,8 @@ import "./metric-customization.css";
 import "./spreadsheet-slide.css";
 import "./measuredSlidingHighlights";
 import "./remote-polish";
+import "./remote-period-dropdown";
+import "./remote-layout-hotfix.css";
 import { DisplayErrorBoundary } from "./components/DisplayErrorBoundary";
 
 createRoot(document.getElementById("root")).render(<StrictMode><DisplayErrorBoundary><PresentationControllerProvider><App /></PresentationControllerProvider></DisplayErrorBoundary></StrictMode>);
