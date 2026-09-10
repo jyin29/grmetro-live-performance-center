7dc0a14c1297e39995ae45a79027fa70404104d9
JYStudios <joshuayin29@gmail.com>
2026-09-10 16:05:25 -0400

    Prefer native GRMetro launcher executable

--- GRMetro Performance Center.cmd
+++ GRMetro Performance Center.cmd
@@ -1,4 +1,12 @@
 @echo off
+setlocal
 cd /d "%~dp0"
-start "" powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\windows\performance-center-launcher.ps1" -AutoStart
+if not exist "%~dp0GRMetro Performance Center.exe" (
+  powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\windows\build-launcher-exe.ps1" >nul 2>nul
+)
+if exist "%~dp0GRMetro Performance Center.exe" (
+  start "" "%~dp0GRMetro Performance Center.exe"
+) else (
+  start "" powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\windows\performance-center-launcher.ps1" -AutoStart
+)
 exit /b 0
